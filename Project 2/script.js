5018cd29576e2ee9fdf7b8beeeea49baca879666
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2022-03-20 23:23:12 -0700

    Add addition feature to calculator

--- Project 2/script.js
+++ Project 2/script.js
@@ -13,6 +13,18 @@ document.querySelector('.calculator-buttons').addEventListener('click', (event) 
             numberStack.pop();
             printScreenState();
         }
+        else if (getBtnValue === '=') {
+            let getResult = add(3, 4);
+            let getOperatorPositions = numberStack.findIndex(operator => operator === "+");
+            let leftEqu = numberStack;
+            let rightEqu = numberStack;
+            let getInt1 = parseInt(leftEqu.slice(0, getOperatorPositions).join(''));
+            let getInt2 = parseInt(rightEqu.slice(getOperatorPositions + 1).join(''));
+            console.log(`${getInt1} + ${getInt2}`);
+            getResult = add(getInt1, getInt2);
+            numberStack = [`${getResult}`];
+            printScreenState();
+        }
         else if (getBtnValue !== 'C' && getBtnValue !== '←' && getBtnValue !== '=') {
             numberStack[0] === "0" ? numberStack[0] = getBtnValue : numberStack.push(getBtnValue);
             printScreenState();
@@ -22,4 +34,20 @@ document.querySelector('.calculator-buttons').addEventListener('click', (event) 
 
 function printScreenState() {
     getTextOutput.innerText = `${numberStack}`;
+}
+
+let add = (val1, val2) => {
+    return val1 + val2;
+}
+
+let sub = (val1, val2) => {
+    return val1 - val2;
+}
+
+let multi = (val1, val2) => {
+    return val1 * val2;
+}
+
+let divide = (val1, val2) => {
+    return val1 / val2;
 }
