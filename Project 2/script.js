a0f04d470ce801450bfcab754d51de1b337a0b91
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2022-03-20 19:22:43 -0700

    add basic stack operations for the calculator

--- Project 2/script.js
+++ Project 2/script.js
@@ -1,1 +1,25 @@
-console.log("Apples");
+const getTextOutput = document.querySelector('.calculator-screen');
+var numberStack = ["0"];
+var computedResult;
+
+document.querySelector('.calculator-buttons').addEventListener('click', (event) => {
+    if (event.target.tagName === "BUTTON") {
+        let getBtnValue = event.target.innerText;
+        if (getBtnValue === 'C') {
+            numberStack = ["0"];
+            printScreenState();
+        }
+        else if (getBtnValue === '←' && numberStack.length > 1) {
+            numberStack.pop();
+            printScreenState();
+        }
+        else if (getBtnValue !== 'C' && getBtnValue !== '←' && getBtnValue !== '=') {
+            numberStack[0] === "0" ? numberStack[0] = getBtnValue : numberStack.push(getBtnValue);
+            printScreenState();
+        }
+    }
+})
+
+function printScreenState() {
+    getTextOutput.innerText = `${numberStack}`;
+}
