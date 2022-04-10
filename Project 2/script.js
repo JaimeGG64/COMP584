d62638bbd9df363d9415726d4cfcfd82aafb2f45
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2022-04-09 19:14:29 -0700

    Remove image files for HW3 and project files for Project 2

--- Project 2/script.js
+++ Project 2/script.js
@@ -1,43 +1,59 @@
 const getTextOutput = document.querySelector('.calculator-screen');
-var numberStack = ["0"];
-var computedResult;
+var equationStack = ["0"];
 
 document.querySelector('.calculator-buttons').addEventListener('click', (event) => {
     if (event.target.tagName === "BUTTON") {
         let getBtnValue = event.target.innerText;
         if (getBtnValue === 'C') {
-            numberStack = ["0"];
+            equationStack = ["0"];
             printScreenState();
         }
-        else if (getBtnValue === '←' && numberStack.length > 1) {
-            numberStack.pop();
+        else if (getBtnValue === '←' && equationStack.length > 1) {
+            equationStack.pop();
             printScreenState();
         }
         else if (getBtnValue === '=') {
             let getResult;
-            let getOperatorPositions = numberStack.findIndex(
+            let getOperatorPositions = equationStack.findIndex(
                 operator =>
-                    operator === "÷" || operator === "×" || operator === "+" || operator === "−");
-            let leftEqu = numberStack;
-            let rightEqu = numberStack;
+                    operator === "÷" ||
+                    operator === "×" ||
+                    operator === "+" ||
+                    operator === "−");
+            let leftEqu = equationStack;
+            let rightEqu = equationStack;
             let getInt1 = parseInt(leftEqu.slice(0, getOperatorPositions).join(''));
             let getInt2 = parseInt(rightEqu.slice(getOperatorPositions + 1).join(''));
-            numberStack[getOperatorPositions] === "÷" ? getResult = divide(getInt1, getInt2) : getResult;
-            numberStack[getOperatorPositions] === "×" ? getResult = multi(getInt1, getInt2) : getResult;
-            numberStack[getOperatorPositions] === "+" ? getResult = add(getInt1, getInt2) : getResult;
-            numberStack[getOperatorPositions] === "−" ? getResult = sub(getInt1, getInt2) : getResult;
-            numberStack = [`${getResult}`];
+            equationStack[getOperatorPositions] === "÷" ?
+                getResult = divide(getInt1, getInt2) : getResult;
+            equationStack[getOperatorPositions] === "×" ?
+                getResult = multi(getInt1, getInt2) : getResult;
+            equationStack[getOperatorPositions] === "+" ?
+                getResult = add(getInt1, getInt2) : getResult;
+            equationStack[getOperatorPositions] === "−" ?
+                getResult = sub(getInt1, getInt2) : getResult;
+            equationStack = [`${getResult}`];
             printScreenState();
         }
         else if (getBtnValue !== 'C' && getBtnValue !== '←' && getBtnValue !== '=') {
-            numberStack[0] === "0" ? numberStack[0] = getBtnValue : numberStack.push(getBtnValue);
+            equationStack[0] === "0" ? equationStack[0] = getBtnValue : equationStack.push(getBtnValue);
             printScreenState();
         }
     }
 })
 
+// function calculateEquation(fromNumber) {
+//     console.log(fromNumber);
+
+//     let nextNumber = fromNumber - 1;
+
+//     if (nextNumber > 0) {
+//         calculateEquation(nextNumber);
+//     }
+// }
+
 function printScreenState() {
-    getTextOutput.innerText = `${numberStack.join('')}`;
+    getTextOutput.innerText = `${equationStack.join('')}`;
 }
 
 let add = (val1, val2) => {
