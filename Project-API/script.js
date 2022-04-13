3ec99226cf74ba56b05619d162150260bed6c938
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2022-04-13 14:53:30 -0700

    Query REST API with an array

--- Project-API/script.js
+++ Project-API/script.js
@@ -1,20 +1,26 @@
 
-const CN_FACTS_URL = `https://api.chucknorris.io/jokes/random`;
+const categories = ["animal", "career", "celebrity", "dev", "fashion", "food", "history", "money", "movie", "music", "sport", "travel"];
 
+var templateFacts = '';
 
-const promise = fetch(CN_FACTS_URL);
-
-var storeFacts = [];
-
-promise
-    .then(function (response) {
-        console.log("OG Response from API", response);
-        const processingPromise = response.json();
-        return processingPromise;
-    })
-    .then(function (processedResponse) {
-        console.log("Response after .json()", processedResponse)
-        storeFacts.push(processedResponse);
-        console.log(storeFacts);
-    })
-
+categories.forEach((element) => {
+    const promise = fetch(`https://api.chucknorris.io/jokes/random?category=${element}`);
+    promise
+        .then(function (response) {
+            // console.log("OG Response from API", response);
+            const processingPromise = response.json();
+            return processingPromise;
+        })
+        .then(function (processedResponse) {
+            // console.log("Response after .json()", processedResponse)
+            let getContainer = document.querySelector(".cn-facts-wrapper")
+            getContainer.innerHTML += `
+            <div class="fact-box">
+                <div class="fact-box__image"></div>
+                <h2 class="fact-box__date">April 4, 2020</h2>
+                <p class="fact-box__fact">${processedResponse.value}</p>
+            </div>
+            `
+        })
+        .catch((err) => console.log(err));
+})
