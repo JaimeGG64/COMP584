587f2c421683919761cf24fd69f73eb2ba8970de
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2022-04-13 22:28:53 -0700

    Improve styling of webpage and added images to display on the front-end

--- Project-API/script.js
+++ Project-API/script.js
@@ -1,10 +1,36 @@
 
-const categories = ["animal", "career", "celebrity", "dev", "fashion", "food", "history", "money", "movie", "music", "sport", "travel"];
+const queryCollection = [
+    {
+        category: "animal",
+        imageLocation: "images/cn-beard.jpg"
+    },
+    {
+        category: "career",
+        imageLocation: "images/cn-cowboy-1.jpg"
+    },
+    {
+        category: "celebrity",
+        imageLocation: "images/cn-cowboy-2.jpg"
+    },
+    {
+        category: "movie",
+        imageLocation: "images/cn-delta.jpg"
+    },
+    {
+        category: "food",
+        imageLocation: "images/cn-regular.jpg"
+    },
+    {
+        category: "dev",
+        imageLocation: "images/cn-young.jpg"
+    }
+];
 
 var templateFacts = '';
 
-categories.forEach((element) => {
-    const promise = fetch(`https://api.chucknorris.io/jokes/random?category=${element}`);
+queryCollection.forEach((element) => {
+    const promise = fetch(`https://api.chucknorris.io/jokes/random?category=${element.category
+        }`);
     promise
         .then(function (response) {
             // console.log("OG Response from API", response);
@@ -16,8 +42,7 @@ categories.forEach((element) => {
             let getContainer = document.querySelector(".cn-facts-wrapper")
             getContainer.innerHTML += `
             <div class="fact-box">
-                <div class="fact-box__image"></div>
-                <h2 class="fact-box__date">April 4, 2020</h2>
+                <img class="fact-box__image" src="${element.imageLocation}" />
                 <p class="fact-box__fact">${processedResponse.value}</p>
             </div>
             `
